9e0a092ad8a06d980ab42d1b5b94bca62d2c7ec9
Luligabi1 <luligabi111@gmail.com>
2021-06-06 01:26:43 -0300

    Added KeyBindingRegistry

--- src/main/java/me/luligabi/cadentis/CadentisClient.java
+++ src/main/java/me/luligabi/cadentis/CadentisClient.java
@@ -1,50 +1,36 @@
 package me.luligabi.cadentis;
 
+import me.luligabi.cadentis.registry.KeyBindingRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.LiteralText;
-import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class CadentisClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        lessBrightnessKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.cadentis.less_brightness",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_LEFT,
-                "category.cadentis.keys"
-        ));
-
-        moreBrightnessKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.cadentis.more_brightness",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_RIGHT,
-                "category.cadentis.keys"
-        ));
+        KeyBindingRegistry.init();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (lessBrightnessKeyBinding.wasPressed()) {
+            while(KeyBindingRegistry.lessBrightnessKeyBinding.wasPressed()) {
                 if(client.options.gamma > -150) {
                     client.options.gamma -= 10;
+                    client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1f));
                     client.player.sendMessage(new LiteralText(client.options.gamma + ""), false);
                 }
             }
-            while (moreBrightnessKeyBinding.wasPressed()) {
+            while(KeyBindingRegistry.moreBrightnessKeyBinding.wasPressed()) {
                 if(client.options.gamma < 150) {
                     client.options.gamma += 10;
+                    client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1f));
                     client.player.sendMessage(new LiteralText(client.options.gamma + ""), false);
                 }
             }
         });
     }
-
-    private static KeyBinding lessBrightnessKeyBinding;
-    private static KeyBinding moreBrightnessKeyBinding;
 }
---
+++ src/main/java/me/luligabi/cadentis/registry/KeyBindingRegistry.java
@@ -0,0 +1,13 @@
+package me.luligabi.cadentis.registry;
+
+import net.minecraft.client.option.KeyBinding;
+
+public class KeyBindingRegistry {
+
+    public static void init() {
+
+    }
+
+    public static KeyBinding lessBrightnessKeyBinding;
+    public static KeyBinding moreBrightnessKeyBinding;
+}
